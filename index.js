6179cd1f3feb4db98ac7a2cc842790ba047b353b
DaffierSquare2 <63510648+DaffierSquare2@users.noreply.github.com>
2023-06-14 15:37:46 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const timeOfMeeting = {
+    year: 2023,
+    month: 7,
+    day: 16
+}
+
+const calculateTime = (year, month, day) => {
+    const currentTime = {
+        year: new Date().getFullYear(),
+        month: new Date().getMonth() + 1,
+        day: new Date().getDate()
+    }
+
+    let formattedMonths = month - currentTime.month;
+    let formattedDays = day - currentTime.day;
+
+    if (formattedMonths < 0) {
+        formattedMonths = -formattedMonths;
+    }
+
+    if (formattedDays < 0) {
+        formattedDays = -formattedDays;
+    }
+
+    return `До встречи осталось: ${year - currentTime.year} лет, ${formattedMonths} месяцев, ${formattedDays} дней`
+}
+
+console.log(calculateTime(timeOfMeeting.year, timeOfMeeting.month, timeOfMeeting.day));
